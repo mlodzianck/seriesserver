9802379d01d9d2a7a7cb0eb214b12cbbdd6a506d
Maciej Tokarski <maciej.tokarski@decerto.pl>
2016-03-16 13:20:00 +0100

    timeouts

--- src/mochitest.erl
+++ src/mochitest.erl
@@ -12,7 +12,7 @@
 %% API
 -compile(export_all).
 start() ->
-  mochiweb_http:start([{name, ?MODULE}, {loop, ?LOOP} | [{port,9999}]]).
+  mochiweb_http:start([{name, ?MODULE}, {loop, ?LOOP} | [{port, 9900}]]).
 
 
 
@@ -22,10 +22,10 @@ loop(Req) ->
     case Req:get(method) of
       Method when Method =:= 'GET'; Method =:= 'HEAD' ->
         case Path of
-          "generate/"++What ->
-            io:format("~p",[What]),
+          "generate/" ++ What ->
+            io:format("~p", [What]),
             QueryStringData = Req:parse_qs(),
-            handle_generate_request(Req,What,QueryStringData);
+            handle_generate_request(Req, What, QueryStringData);
           _ ->
             Req:serve_file(Path, "./")
         end;
@@ -38,13 +38,43 @@ loop(Req) ->
         Req:respond({501, [], []})
     end
   catch
-    Why:Err-> error_logger:info_msg("Exception ~p ~p",[Why,Err])
+    Why:Err -> error_logger:info_msg("Exception ~p ~p", [Why, Err])
   end.
 
 
-handle_generate_request(Req,"erlang.json", Proplist) ->
-  R = [rnd_gen:rnd_erlang(2.0,1) || _X <- lists:seq(0,100000)],
-  Req:respond({200, [{"Content-Type", "application/json"}], [mochijson2:encode(R)]});
+handle_generate_request(Req, "erlang.json", Proplist) ->
+  {ok, Pid} = rnd_gen:start(),
+  Series = (catch rnd_gen:rnd_erlang(Pid, 2.0, 5, 10000000, json, 1000)),
+  send_http_response_and_terminate_process(Series,Req,Pid);
 
-handle_generate_request(Req,_,_) ->
+
+handle_generate_request(Req, _, _) ->
   Req:respond({400, [], "400 Bad Request\r\n"}).
+
+send_http_response_and_terminate_process(Series,Req,GeneratorPid) ->
+  send_series_http_response(Series, Req),
+  terminate_generator_process(Series,GeneratorPid).
+
+
+send_series_http_response(Series, Req) ->
+  Response = case Series of
+               Reply when is_list(Reply) ->
+                 {200, [{"Content-Type", "application/json"}], [Reply]};
+               {'EXIT', {timeout, _}} ->
+                 error_logger:warning_msg("Got timeout message", []),
+                 {503, [], "503 Timeout\r\n"};
+               Other ->
+                 error_logger:error_msg("Got other message ~p", [Other]),
+                 {503, [], "503 Timeout\r\n"}
+             end,
+  Req:respond(Response).
+
+terminate_generator_process(Series, GenPid) ->
+  case Series of
+    Reply when is_list(Reply) ->
+      rnd_gen:stop(GenPid);
+    {'EXIT', {timeout, _}} ->
+      erlang:exit(GenPid, kill);
+    _ ->
+      erlang:exit(GenPid, kill)
+  end.
--- src/rnd_gen.erl
+++ src/rnd_gen.erl
@@ -1,18 +1,10 @@
-%%%-------------------------------------------------------------------
-%%% @author maciek
-%%% @copyright (C) 2016, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 14. mar 2016 15:34
-%%%-------------------------------------------------------------------
 -module(rnd_gen).
 -author("maciek").
 
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+
 
 %% gen_server callbacks
 -export([init/1,
@@ -29,139 +21,85 @@
 %%%===================================================================
 %%% API
 %%%===================================================================
--export([rnd_expotential/1,rnd_erlang/2,rnd_uniform/2]).
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(start_link() ->
-  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
-start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
-rnd_expotential(Alpha) ->
-  gen_server:call({rnd_exponential,[{alpha,Alpha}]}).
-rnd_erlang(B,C) ->
-  gen_server:call(?SERVER,{rnd_erlang,[{b,B},{c,C}]}).
-rnd_uniform(Min,Max) ->
-  gen_server:call(?SERVER,{rnd_uniform,[{min,Min},{max,Max}]}).
+-export([start/0]).
+-export([rnd_expotential/4,rnd_erlang/5,rnd_uniform/5]).
+-export([rnd_expotential/5,rnd_erlang/6,rnd_uniform/6]).
+-export([stop/1]).
+
+
+start() ->
+  gen_server:start(?MODULE, [], []).
+rnd_expotential(Pid,Alpha,SeriesLen,Format) ->
+  rnd_expotential(Pid,Alpha,SeriesLen,Format,infinity).
+
+rnd_erlang(Pid,B,C,SeriesLen,Format) ->
+  rnd_erlang(Pid,B,C,SeriesLen,Format,infinity).
+
+rnd_uniform(Pid,Min,Max,Format,SeriesLen) ->
+  rnd_uniform(Pid,Min,Max,SeriesLen,Format,infinity).
+
+
+rnd_expotential(Pid,Alpha,SeriesLen,Format,Timeout) ->
+  gen_server:call(Pid,{rnd_exponential,[{alpha,Alpha}],Format,SeriesLen},Format,Timeout).
+
+rnd_erlang(Pid,B,C,SeriesLen,Format,Timeout) ->
+  gen_server:call(Pid,{rnd_erlang,[{b,B},{c,C}],Format,SeriesLen},Timeout).
+
+rnd_uniform(Pid,Min,Max,SeriesLen,Format,Timeout) ->
+  gen_server:call(Pid,{rnd_uniform,[{min,Min},{max,Max}],Format,SeriesLen},Timeout).
+
+stop(Pid) ->
+  gen_server:call(Pid,stop).
 
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
--spec(init(Args :: term()) ->
-  {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term()} | ignore).
+
 init([]) ->
   rand:seed(?RAND_ALGO,erlang:now()),
   {ok, #state{}}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
-    State :: #state{}) ->
-  {reply, Reply :: term(), NewState :: #state{}} |
-  {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
-  {stop, Reason :: term(), NewState :: #state{}}).
-handle_call({rnd_exponential,{alpha,Alpha}}, _From, State) when  not is_float(Alpha); Alpha<0 ->
-  {reply, {error,parameter_invalid}, State};
-handle_call({rnd_exponential,[{alpha,Alpha}]}, _From, State) ->
-  R = -((math:log(1-rand:uniform()))/Alpha),
-  {reply, R, State};
 
-handle_call({rnd_erlang,[{b,B},{c,C}]}, _From, State) when not is_integer(C) ; C<1; not is_float(B); B<0->
+handle_call({rnd_exponential,{alpha,Alpha},_,_}, _From, State) when  not is_float(Alpha); Alpha<0 ->
   {reply, {error,parameter_invalid}, State};
-handle_call({rnd_erlang,[{b,B},{c,C}]}, _From, State) ->
-  R  = generate_erlang_radnom_number(B,C),
-  {reply, R, State};
+handle_call({rnd_exponential,[{alpha,Alpha}],Format,SeriesLen}, _From, State) ->
+  R = [-((math:log(1-rand:uniform()))/Alpha) || _X <- lists:seq(0,SeriesLen)],
+  {reply, format_convert(R,Format), State};
 
-handle_call({rnd_uniform,[{min,Min},{max,Max}]}, _From, State) when not is_float(Min); not is_float(Max);Max=<Min ->
+
+handle_call({rnd_erlang,[{b,B},{c,C}],_,_}, _From, State) when not is_integer(C) ; C<1; not is_float(B); B<0->
   {reply, {error,parameter_invalid}, State};
-handle_call({rnd_uniform,[{min,Min},{max,Max}]}, _From, State) ->
-  R  = random:uniform()*(Max-Min) + Min,
-  {reply, R, State};
+handle_call({rnd_erlang,[{b,B},{c,C}],Format,SeriesLen}, _From, State) ->
+  R  = [generate_erlang_radnom_number(B,C) || _X <- lists:seq(0,SeriesLen)],
+  {reply, format_convert(R,Format), State};
+
+handle_call({rnd_uniform,[{min,Min},{max,Max}],_,_}, _From, State) when not is_float(Min); not is_float(Max);Max=<Min ->
+  {reply, {error,parameter_invalid}, State};
+handle_call({rnd_uniform,[{min,Min},{max,Max}],Format,SeriesLen}, _From, State) ->
+  R  = [random:uniform()*(Max-Min) + Min || _X <- lists:seq(0,SeriesLen)],
+  {reply, format_convert(R,Format), State};
+
+handle_call(stop, _From, State) ->
+  {stop,normal,ok,State};
+
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_cast(Request :: term(), State :: #state{}) ->
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), NewState :: #state{}}).
+
 handle_cast(_Request, State) ->
   {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), NewState :: #state{}}).
+
 handle_info(_Info, State) ->
   {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
--spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
-    State :: #state{}) -> term()).
-terminate(_Reason, _State) ->
+
+terminate(Reason, _State) ->
+  error_logger:info_msg("Terminating rnd_gen server due to ~p",[Reason]),
   ok.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
--spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
-    Extra :: term()) ->
-  {ok, NewState :: #state{}} | {error, Reason :: term()}).
+
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
@@ -185,4 +123,8 @@ generate_erlang_radnom_number(B,1,Acc) ->
 generate_erlang_radnom_number(B,C,Acc) ->
   generate_erlang_radnom_number(B,C-1,Acc*rand:uniform()).
 
-
+format_convert(Series,Format) ->
+  case Format of
+    json ->  mochijson2:encode(Series);
+    _ ->  Series
+  end.
